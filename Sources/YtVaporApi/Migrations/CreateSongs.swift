e90cbbca30bfe4984d88d1d0ef96ff32f7a11539
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-07 03:02:25 +0300

    Add CreateSongs migration to define songs table schema

--- Sources/YtVaporApi/Migrations/CreateSongs.swift
+++ Sources/YtVaporApi/Migrations/CreateSongs.swift
@@ -1,8 +1,23 @@
 //
-//  File.swift
+//  CreateSongs.swift
 //  YtVaporApi
 //
 //  Created by Abdelrahman Mohamed on 06.02.2026.
 //
 
-import Foundation
+import Fluent
+
+struct CreateSongs: AsyncMigration {
+    func prepare(on database: any Database) async throws {
+        try await database.schema("songs")
+            .id()
+            .field("title", .string, .required)
+            .field("artist", .string)
+            .create()
+    }
+    
+    func revert(on database: any Database) async throws {
+        try await database.schema("songs")
+            .delete()
+    }
+}
