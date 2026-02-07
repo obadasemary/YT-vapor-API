ef23a429c574441f9b1340ff9c13f599e8229981
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-07 03:02:30 +0300

    Add Song model with properties and DTO conversion

--- Sources/YtVaporApi/Models/Song.swift
+++ Sources/YtVaporApi/Models/Song.swift
@@ -1,8 +1,38 @@
 //
-//  File.swift
+//  Song.swift
 //  YtVaporApi
 //
 //  Created by Abdelrahman Mohamed on 06.02.2026.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+final class Song: Model, Content, @unchecked Sendable {
+    static let schema = "songs"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "title")
+    var title: String
+    
+    @Field(key: "artist")
+    var artist: String?
+    
+    init() {}
+    
+    init(id: UUID? = nil, title: String, artist: String? = nil) {
+        self.id = id
+        self.title = title
+        self.artist = artist
+    }
+    
+    func toDTO() -> SongDTO {
+        .init(
+            id: self.id,
+            title: self.$title.value,
+            artist: self.$artist.value ?? nil
+        )
+    }
+}
