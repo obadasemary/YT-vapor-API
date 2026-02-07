3af8a0628b93e7c2ae3d37a33bdf72580565e51f
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-07 03:02:04 +0300

    Replace Todo migration with Songs migration in configuration

--- Sources/YtVaporApi/configure.swift
+++ Sources/YtVaporApi/configure.swift
@@ -17,8 +17,10 @@ public func configure(_ app: Application) async throws {
         tls: .prefer(try .init(configuration: .clientDefault)))
     ), as: .psql)
 
-    app.migrations.add(CreateTodo())
+    app.migrations.add(CreateSongs())
+    try app.autoMigrate().wait()
 
     // register routes
     try routes(app)
 }
+//app.migrations.add(CreateTodo())
