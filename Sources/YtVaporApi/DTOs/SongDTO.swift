0f30ef41b18d49bdb6aec7365f64f2753a2cae40
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-07 03:02:20 +0300

    Add SongDTO struct to define song data transfer object

--- Sources/YtVaporApi/DTOs/SongDTO.swift
+++ Sources/YtVaporApi/DTOs/SongDTO.swift
@@ -1,8 +1,28 @@
 //
-//  File.swift
+//  SongDTO.swift
 //  YtVaporApi
 //
 //  Created by Abdelrahman Mohamed on 07.02.2026.
 //
 
-import Foundation
+import Fluent
+import Vapor
+
+struct SongDTO: Content {
+    var id: UUID?
+    var title: String?
+    var artist: String?
+    
+    func toModel() -> Song {
+        let model = Song()
+        
+        model.id = self.id
+        if let title = self.title {
+            model.title = title
+        }
+        if let artist = self.artist {
+            model.artist = artist
+        }
+        return model
+    }
+}
