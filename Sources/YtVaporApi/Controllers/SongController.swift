27c25e5259fd9b845b65a7a9d1050a4ea8c54540
Abdelrahman Mohamed <obada.semary@gmail.com>
2026-02-08 04:16:20 +0300

    Add PUT /songs/:songID update endpoint

--- Sources/YtVaporApi/Controllers/SongController.swift
+++ Sources/YtVaporApi/Controllers/SongController.swift
@@ -14,6 +14,8 @@ struct SongController: RouteCollection {
         
         songs.get(use: index)
         songs.post(use: create)
+        let song = songs.grouped(":songID")
+        song.put(use: update)
     }
     
     @Sendable
@@ -24,6 +26,18 @@ struct SongController: RouteCollection {
     @Sendable
     func create(req: Request) async throws -> SongDTO {
         let song = try req.content.decode(SongDTO.self).toModel()
+        try await song.save(on: req.db)
+        return song.toDTO()
+    }
+
+    @Sendable
+    func update(req: Request) async throws -> SongDTO {
+        guard let song = try await Song.find(req.parameters.get("songID"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        let dto = try req.content.decode(SongDTO.self)
+        if let title = dto.title { song.title = title }
+        if let artist = dto.artist { song.artist = artist }
         try await song.save(on: req.db)
         return song.toDTO()
     }
